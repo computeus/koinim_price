9fda1285514dec7137ee294feba875e12305bde2
Fatih AYTAÇ <fatih.aytac@gmail.com>
2017-02-19 22:58:26 +0300

    Added orderbook and ticker classes.
    This will parse the orderbook and ticker data from the exchange api.

---
+++ lib/koinim_price/orderbook.rb
@@ -0,0 +1,27 @@
+module KoinimPrice
+  class Orderbook
+    require 'httparty'
+
+    API_URL = "#{KOINIM_URL}orderbook"
+    @response = nil
+
+    def self.asks
+      response['asks']
+    end
+
+    def self.bids
+      response['bids']
+    end
+
+    def self.refresh
+      @response = nil
+      response
+      true
+    end
+
+    private
+    def self.response
+      @response ||= JSON.parse(HTTParty.get(API_URL).body)
+    end
+  end
+end
---
+++ lib/koinim_price/ticker.rb
@@ -0,0 +1,49 @@
+module KoinimPrice
+  class Ticker
+    require 'httparty'
+    require 'json'
+
+    API_URL = "#{KOINIM_URL}ticker"
+
+    @response = nil
+
+    def self.buy_price
+      response['buy']
+    end
+
+    def self.sell_price
+      response['sell']
+    end
+
+    def self.volume
+      response['volume']
+    end
+
+    def self.average_price
+      response['avg']
+    end
+
+    def self.weighted_average_price
+      response['wavg']
+    end
+
+    def self.highest
+      response['high']
+    end
+
+    def self.lowest
+      response['low']
+    end
+
+    def self.refresh
+      @response = nil
+      response
+      true
+    end
+
+    private
+    def self.response
+      @response ||= JSON.parse(HTTParty.get(API_URL).body)
+    end
+  end
+end
